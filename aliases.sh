0713f33307c88b4c38c68a2222ceaf4f5b5f98b8
piterio <pedro.tallon.castro@gmail.com>
2019-12-17 15:45:56 +0100

    Add aliases to increase productivity

---
+++ aliases.sh
@@ -0,0 +1,14 @@
+# Jumps
+alias ..="cd .."
+alias ...="cd ../.."
+alias ~="cd ~"
+alias code='cd ~/code'
+alias work="cd ~/code/work"
+
+# Brew
+alias brwe="brew"
+alias bsl="brew services list"
+
+# Others
+alias c='pbcopy'
+alias tf='tail -f'
